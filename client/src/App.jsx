4de09294e83b2bd65d22c360b5f1c414ab2b653a
Student Shruti <shrutisharma87860@gmail.com>
2026-09-22 23:18:35 +0530

    Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
       formData.append('resume', resumeFile); 
       formData.append('jobDescription', jobDescription); 
       const response = await fetch( 
-        'https://ai-resume-analyzer-api-2vqz.onrender.com/api/analyze', 
+        'https://ai-resume-analyzer-api-2vqz.onrender.com', 
         { 
           method: 'POST', 
           body: formData, 
